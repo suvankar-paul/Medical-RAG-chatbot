e583f869ce558952ef800cd54e7ac7e514fe73b5
Suvankar <suvankarpaul004@gmail.com>
2026-01-21 21:04:44 +0530

    rag pipeline complete

---
+++ src/helper.py
@@ -0,0 +1,59 @@
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from typing import List
+from langchain.schema import Document
+from langchain.embeddings import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+import os
+from langchain_openai import ChatOpenAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+load_dotenv()
+# Extract text from PDF files
+def load_pdf_files(data):
+    loader = DirectoryLoader(
+        data,
+        glob="*.pdf",
+        loader_cls=PyPDFLoader
+    )
+
+    documents = loader.load()
+    return documents
+
+def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
+    """
+    Given a list of Document objects, return a new list of Document objects
+    containing only 'source' in metadata and the original page_content.
+    """
+    minimal_docs: List[Document] = []
+    for doc in docs:
+        src = doc.metadata.get("source")
+        minimal_docs.append(
+            Document(
+                page_content=doc.page_content,
+                metadata={"source": src}
+            )
+        )
+    return minimal_docs
+
+def text_split(minimal_docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=20,
+    )
+    texts_chunk = text_splitter.split_documents(minimal_docs)
+    return texts_chunk
+
+def download_embeddings():
+    """
+    Download and return the HuggingFace embeddings model.
+    """
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = HuggingFaceEmbeddings(
+        model_name=model_name
+    )
+    return embeddings
+
+embedding = download_embeddings()
